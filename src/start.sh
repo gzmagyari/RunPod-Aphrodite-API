f572105b3b10c93f03ee5c00ab84435a8f06cfa4
gaborszenesi <gaborszenesi@gmail.com>
2024-07-28 17:22:21 +0100

    fix

--- src/start.sh
+++ src/start.sh
@@ -6,7 +6,7 @@ CMD="python3 -m aphrodite.endpoints.openai.api_server
              --host 0.0.0.0
              --port 4444
              --model /workspace/models/blackroot-8B-V1_q8_0.gguf
-             --q gguf
+             --quantization gguf
              --tokenizer bluuwhale/L3-SthenoMaidBlackroot-8B-V1
              --served-model-name blackroot-8B-V1"
 
