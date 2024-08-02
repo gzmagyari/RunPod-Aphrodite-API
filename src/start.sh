08b21aa4b12494faddee0f23837b2f78ddf2946b
gaborszenesi <gaborszenesi@gmail.com>
2024-08-02 17:44:54 +0100

    test

--- src/start.sh
+++ src/start.sh
@@ -8,18 +8,9 @@ umask 002
 # Enable command tracing
 set -x
 
-# Check if a parameter is passed to determine if it should run locally
-if [ "$1" = "local" ]; then
-    API_HOST="0.0.0.0"
-    HANDLER_CMD="python -u /handler.py --rp_serve_api --rp_api_host='0.0.0.0' --rp_api_port 8000"
-else
-    API_HOST="127.0.0.1"
-    HANDLER_CMD="python -u /handler.py"
-fi
-
 # Start the Aphrodite Engine API server in the background
 python3 -m aphrodite.endpoints.openai.api_server \
-    --host $API_HOST \
+    --host 0.0.0.0 \
     --port 4444 \
     --model /workspace/models/blackroot-8B-V1_q8_0.gguf \
     --quantization gguf \
@@ -30,4 +21,4 @@ python3 -m aphrodite.endpoints.openai.api_server \
 sleep 1
 
 # Start the RunPod handler
-exec $HANDLER_CMD
+exec python -u /handler.py --rp_serve_api --rp_api_host='0.0.0.0' --rp_api_port 8000
