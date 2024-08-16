0ae019bdda5b95c8d09863c6aeac4ecf44bd180e
gaborszenesi <gaborszenesi@gmail.com>
2024-08-16 01:37:27 +0100

    test

--- src/start.sh
+++ src/start.sh
@@ -14,7 +14,6 @@ if [ "$1" = "local" ]; then
         --host 0.0.0.0 \
         --port 4444 \
         --model Orenguteng/Llama-3.1-8B-Lexi-Uncensored-V2 \
-        --quantization gguf \
         --tokenizer Orenguteng/Llama-3.1-8B-Lexi-Uncensored-V2 \
         --served-model-name Llama-3.1-8B-Lexi &
 else
@@ -22,7 +21,6 @@ else
         --host 127.0.0.1 \
         --port 4444 \
         --model /workspace/models/Llama-3.1-8B-Lexi-Uncensored_V2_Q8.gguf \
-        --quantization gguf \
         --tokenizer Orenguteng/Llama-3.1-8B-Lexi-Uncensored-V2 \
         --served-model-name Llama-3.1-8B-Lexi &
 fi
