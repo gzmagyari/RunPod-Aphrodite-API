f018280a3b02a05f7703063c43c1b812113dce62
gaborszenesi <gaborszenesi@gmail.com>
2024-08-31 10:48:17 +0100

    test

--- src/start.sh
+++ src/start.sh
@@ -13,9 +13,7 @@ if [ "$1" = "local" ]; then
     python3 -m aphrodite.endpoints.openai.api_server \
         --host 0.0.0.0 \
         --port 4447 \
-        --model /workspace/models/xLAM-1b-fc-r/xLAM-1b-fc-r.Q8_0.gguf \
-        --quantization gguf \
-        --tokenizer Salesforce/xLAM-1b-fc-r \
+        --model Salesforce/xLAM-1b-fc-r \
         --gpu-memory-utilization 0.3 \
         --max-model-len 2048 \
         --served-model-name xLAM-1b-fc-r.Q8_0 &
